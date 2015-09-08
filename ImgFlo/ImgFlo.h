e1daa0aba2e38b104cb88fe9a5a060d6808154c6
Paul Young <paulyoungonline@gmail.com>
2015-09-08 16:50:41 -0700

    Initial Commit

---
+++ ImgFlo/ImgFlo.h
@@ -0,0 +1,19 @@
+//
+//  ImgFlo.h
+//  ImgFlo
+//
+//  Created by Paul Young on 9/8/15.
+//  Copyright (c) 2015 The Grid. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ImgFlo.
+FOUNDATION_EXPORT double ImgFloVersionNumber;
+
+//! Project version string for ImgFlo.
+FOUNDATION_EXPORT const unsigned char ImgFloVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ImgFlo/PublicHeader.h>
+
+
