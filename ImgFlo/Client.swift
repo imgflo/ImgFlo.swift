644b66a8435a0fb5e2e58e87d4fbc6cc7b4b23e7
Paul Young <paulyoungonline@gmail.com>
2015-09-10 21:21:35 -0700

    Use guards.

--- ImgFlo/Client.swift
+++ ImgFlo/Client.swift
@@ -13,42 +13,41 @@ public struct Client {
     }
     
     public func getURL(graph: Graph, _ URLString: String, _ format: String? = nil) -> NSURL? {
-        if let components = NSURLComponents(string: server) {
-
-            let input = NSURLQueryItem(name: "input", value: URLString)
-            components.queryItems = [ input ] + graph.queryItems
-        
-            if let query = components.percentEncodedQuery {
-                let derivedFormat: String?
-
-                if let providedFormat = format {
-                    derivedFormat = providedFormat
-                } else if let pathExtension = NSURL(string: URLString)?.pathExtension where !pathExtension.isEmpty {
-                    derivedFormat = pathExtension.lowercaseString == "jpg:large" ? "jpg" : pathExtension
-                } else {
-                    derivedFormat = nil
-                }
-                
-                let graphNameWithFormat: String
-                
-                if let format = derivedFormat {
-                    graphNameWithFormat = graph.rawValue + "." + format.lowercaseString
-                } else {
-                    graphNameWithFormat = graph.rawValue
-                }
-                
-                if let token = "\(graphNameWithFormat)?\(query)\(secret)".MD5 {
-                    components.path = "/" + ([ "graph", key, token, graphNameWithFormat ]).joinWithSeparator("/")
-                } else {
-                    return .None
-                }
-            } else {
-                return .None
-            }
-            
-            return components.URL
-        } else {
+        guard let components = NSURLComponents(string: server) else {
             return .None
         }
+        
+        let input = NSURLQueryItem(name: "input", value: URLString)
+        components.queryItems = [ input ] + graph.queryItems
+        
+        guard let query = components.percentEncodedQuery else {
+            return .None
+        }
+        
+        let derivedFormat: String?
+
+        if let providedFormat = format {
+            derivedFormat = providedFormat
+        } else if let pathExtension = NSURL(string: URLString)?.pathExtension where !pathExtension.isEmpty {
+            derivedFormat = pathExtension.lowercaseString == "jpg:large" ? "jpg" : pathExtension
+        } else {
+            derivedFormat = nil
+        }
+
+        let graphNameWithFormat: String
+
+        if let format = derivedFormat {
+            graphNameWithFormat = graph.rawValue + "." + format.lowercaseString
+        } else {
+            graphNameWithFormat = graph.rawValue
+        }
+        
+        guard let token = "\(graphNameWithFormat)?\(query)\(secret)".MD5 else {
+            return .None
+        }
+        
+        components.path = "/" + ([ "graph", key, token, graphNameWithFormat ]).joinWithSeparator("/")
+        
+        return components.URL
     }
 }
